a7835eb3bdcbf003415fbb08ccbec03980bae33d
anuyogamlab <61569349+anuyogamlab@users.noreply.github.com>
2023-07-24 16:25:14 -0400

    Update wrapper.py

--- Script/wrapper.py
+++ Script/wrapper.py
@@ -2,8 +2,8 @@ from google.cloud.storage.client import Client
 import papermill as pm
 import sys
 
-gcspath_input_notebook="gs://databricks-enablement/GEP/notebooktest/scala_test.ipynb"
-gcspath_output_notebook="gs://databricks-enablement/GEP/notebooktest/scala_output.ipynb"
+gcspath_input_notebook="gs://{input bucket}/xmlparser.ipynb"
+gcspath_output_notebook="gs://{input bucket}/scala_output.ipynb"
 
 # pm.execute_notebook(
 #       gcspath_input_notebook, gcspath_output_notebook, kernel_name='python3',log_output=True, progress_bar=False, stdout_file=sys.stdout, parameters=params, **kwargs)
